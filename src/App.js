ea3f9f97deeb28b886cff952d461c4fa181f7bfd
juhokim <line1559@naver.com>
2024-10-15 13:33:14 +0900

    change setting

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';  // react-router-dom 최신 버전 사용
 import MainScreen from './MainScreen';  // default import로 MainScreen 컴포넌트 불러오기
 import NextPage from './NextPage';  // default import로 NextPage 컴포넌트 불러오기
+import ImagePage from './ImagePage';
 
 const App = () => {
   return (
--- src/ImagePage.js
+++ src/ImagePage.js
@@ -4,14 +4,15 @@ import './ImagePage.css';  // 스타일 파일
 const ImagePage = ({ onClose }) => {
   return (
     <div className="modalImageOverlay">
-      <div className='modalImageContent'>
+      <div className="modalImageContent">
+        {/* 제목 추가 */}
+        <div className="modalImageTitle">생성된 이미지</div>
         {/* 이미지 화면 */}
         <div className="imagePreview">
-          <p>생성된 이미지</p>
           <img src={require('./image/imageExample.jpeg')} alt="생성된 이미지" />  {/* 이미지 파일 경로 수정 */}
-          {/* 확인 버튼 */}
-          <button className="confirmButton" onClick={onClose}>확인</button>
         </div>
+        {/* 확인 버튼 */}
+        <button className="confirmButton" onClick={onClose}>확인</button>
       </div>
     </div>
   );
