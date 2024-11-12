668fd56f62e68f5a1302bfff5bc4771ba9146cbf
juhokim <line1559@naver.com>
2024-11-12 14:22:42 +0900

    imagePage change

--- src/ImagePage.js
+++ src/ImagePage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './css/ImagePage.css';
 import { useNavigate } from 'react-router-dom';
 import Draggable from 'react-draggable';
+import html2canvas from 'html2canvas';
 
 const ImagePage = ({ pdfSummary, onClose }) => {
   const navigate = useNavigate();
@@ -16,6 +17,20 @@ const ImagePage = ({ pdfSummary, onClose }) => {
 
   const handleConfirmClick = () => {
     navigate('/send-message');
+  };
+
+  const handleSaveImage = () => {
+    const imageContainer = document.querySelector('.imageContainer');
+
+    html2canvas(imageContainer, {
+      backgroundColor: null,
+      useCORS: true,
+    }).then((canvas) => {
+      const link = document.createElement('a');
+      link.href = canvas.toDataURL('image/png');
+      link.download = 'customized_image.png';
+      link.click();
+    });
   };
 
   const handleColorChange = (index, color) => {
@@ -35,58 +50,62 @@ const ImagePage = ({ pdfSummary, onClose }) => {
   return (
     <div className="modalImageOverlay">
       <div className="modalImageContent">
-        <div className="modalImageTitle">커스터마이즈된 포스터</div>
-
-        <div className="imagePreview">
-          <img src={require('./image/imageExample.png')} alt="생성된 이미지" />
-          {/* 각 문장을 스티커처럼 드래그 가능한 요소로 만듭니다 */}
-          {splitSummary.map((sentence, index) => (
-            <Draggable key={index} bounds="parent">
-              <div
-                className="placedText"
-                style={{
-                  color: styles[index]?.color || '#000',
-                  fontSize: styles[index]?.fontSize || '16px',
-                  position: 'absolute',
-                  transform: 'translate(-50%, -50%)',
-                  cursor: 'move',
-                  zIndex: 10,
-                }}
-              >
-                {sentence}
+        <h3>커스터마이즈된 포스터</h3>
+        
+        <div className="imageContainer">
+          <img 
+            src={require('./image/imageExample.png')} 
+            alt="생성된 이미지" 
+            className="imagePreview" 
+          />
+          {/* Draggable text overlay */}
+          <div className="textOverlay">
+            {splitSummary.map((sentence, index) => (
+              <div key={index} className="textControlGroup">
+                <Draggable bounds=".imagePreview">
+                  <div
+                    className="overlayText"
+                    style={{
+                      color: styles[index]?.color || '#000',
+                      fontSize: styles[index]?.fontSize || '16px',
+                    }}
+                  >
+                    {sentence}
+                  </div>
+                </Draggable>
+                
+                {/* 고정된 글자색과 크기 조절 컨트롤 */}
+                <div className="inlineControls" data-html2canvas-ignore="true">
+                  <label>
+                    색상:
+                    <input
+                      type="color"
+                      onChange={(e) => handleColorChange(index, e.target.value)}
+                    />
+                  </label>
+                  <label>
+                    글자 크기:
+                    <input
+                      defaultValue={16}
+                      type="number"
+                      min="10"
+                      max="100"
+                      onChange={(e) => handleFontSizeChange(index, e.target.value)}
+                    /> px
+                  </label>
+                </div>
               </div>
-            </Draggable>
-          ))}
+            ))}
+          </div>
         </div>
 
-        <button className="imageSaveButton" onClick={onClose}></button>
-        <button className="confirmButton" onClick={handleConfirmClick}>확인</button>
-      </div>
-
-      <div className="summaryContainer">
-        <h3>PDF 요약 내용</h3>
-        {splitSummary.map((sentence, index) => (
-          <div key={index} className="sentenceContainer">
-            <div className="controls">
-              <label>
-                색상:
-                <input
-                  type="color"
-                  onChange={(e) => handleColorChange(index, e.target.value)}
-                />
-              </label>
-              <label>
-                글자 크기:
-                <input
-                  type="number"
-                  min="10"
-                  max="100"
-                  onChange={(e) => handleFontSizeChange(index, e.target.value)}
-                /> px
-              </label>
-            </div>
-          </div>
-        ))}
+        {/* 오른쪽 하단에 위치한 확인과 저장 버튼 */}
+        <div className="buttonContainer">
+          <button className="confirmButton" onClick={handleConfirmClick}>확인</button>
+          <button className="saveButton" onClick={handleSaveImage}>
+            <img src={require('./image/imageDownload.png')} alt="저장" className="downloadIcon" />
+          </button>
+        </div>
       </div>
     </div>
   );
